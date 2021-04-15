b762b3635732764a00b3934213005bf8b01567ed
HakerX-crypto <82603676+HakerX-crypto@users.noreply.github.com>
2021-04-16 00:11:27 +0530

    Create login_script.py

---
+++ login_script.py
@@ -0,0 +1,25 @@
+#!/data/data/com.termux/files/usr/bin/env python
+
+import getpass
+import hashlib
+import sys
+import os
+
+password = getpass.getpass()
+
+filepass = open("/data/data/com.termux/files/usr/share/login/.pass", "r")
+filepass = filepass.read().split("\n")[0]
+
+password = password.encode()
+password = hashlib.sha1(password).hexdigest()
+
+if password != filepass:
+    print("Invalid password")
+    os.system("exit")
+else:
+    prefix = "/data/data/com.termux/files/usr"
+    home = "/data/data/com.termux/files/home"
+    motd = False
+    hush = False
+
+    os.system("clear")
